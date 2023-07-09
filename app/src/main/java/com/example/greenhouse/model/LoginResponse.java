1073e50a96a63775d1ea3bfff3ed947a93018de0
alinusiacoban <aliniacoban@yahoo.com>
2023-07-09 20:08:42 +0200

    hmm

--- app/src/main/java/com/example/greenhouse/model/LoginResponse.java
+++ app/src/main/java/com/example/greenhouse/model/LoginResponse.java
@@ -6,10 +6,6 @@ public class LoginResponse {
     @SerializedName("token")
     private String token;
 
-    public LoginResponse(String token) {
-        this.token = token;
-    }
-
     public String getToken() {
         return token;
     }
