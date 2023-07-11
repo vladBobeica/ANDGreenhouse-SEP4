e06f4cc783a610cf5ba59529754b18eb2118fc2b
alinusiacoban <aliniacoban@yahoo.com>
2023-07-11 13:50:16 +0200

    Styled bottom navbar, added floating action button and settings fragment+vm

--- app/src/main/java/com/example/greenhouse/MainActivity.java
+++ app/src/main/java/com/example/greenhouse/MainActivity.java
@@ -23,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,  R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
