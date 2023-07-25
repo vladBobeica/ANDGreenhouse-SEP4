e17bf7316a90356dcfaa45446730175678420154
Vlad Bobeica <vlad.bobeica@outlook.com>
2023-07-25 19:06:06 +0200

    Add user id distinction for greenhouses (mock only)

--- app/src/main/java/com/example/greenhouse/api/MockApiService.java
+++ app/src/main/java/com/example/greenhouse/api/MockApiService.java
@@ -7,31 +7,41 @@ import android.util.Log;
 import com.example.greenhouse.model.GreenHouseModel;
 import com.example.greenhouse.model.LoginRequest;
 import com.example.greenhouse.model.LoginResponse;
-import com.example.greenhouse.model.mock.MockGreenHouseRepository;
-import com.example.greenhouse.model.mock.MockUserData;
+import com.example.greenhouse.model.MeasurementModel;
+import com.example.greenhouse.model.UserModel;
+import com.example.greenhouse.repository.mock.MockGreenHouseRepository;
+import com.example.greenhouse.repository.mock.MockMeasurementRepository;
+import com.example.greenhouse.repository.mock.MockUserRepository;
 import com.example.greenhouse.utils.RetrofitUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import retrofit2.Call;
 
 public class MockApiService implements ApiService {
+    private static String mockToken;
+
     @Override
     public Call<LoginResponse> login(LoginRequest loginRequest) {
         String email = loginRequest.getEmail();
         String password = loginRequest.getPassword();
 
         // Retrieve the mock user data
-        List<MockUserData.User> users = MockUserData.getUsers();
+        List<UserModel> users = MockUserRepository.getUsers();
 
         // Check if the provided email and password match any user in the mock data
-        for (MockUserData.User user : users) {
-            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+        for (UserModel user : users) {
+            if (user.getEmail().equals(email) && password.equals("111")) {
                 // Authentication successful, return a mock LoginResponse with a token
                 String token = generateMockToken(); // Generate a mock token
                 LoginResponse loginResponse = new LoginResponse(token);
                 Log.d(TAG, "Mock user found");
+
+                // Store token in mock
+                mockToken = String.valueOf(user.getId());
 
                 return RetrofitUtils.createSuccessCall(loginResponse);
             }
@@ -46,7 +56,39 @@ public class MockApiService implements ApiService {
     public Call<List<GreenHouseModel>> getUserGreenHouses(String token) {
         List<GreenHouseModel> mockGreenHouses = MockGreenHouseRepository.getGreenHouses();
 
-        return RetrofitUtils.createSuccessCall(mockGreenHouses);
+        List<GreenHouseModel> usersGreenHouses = new ArrayList<>();
+
+        // Vlad's green houses
+        if (mockToken.equals("1")) {
+            usersGreenHouses.addAll(
+                    Arrays.asList(
+                            mockGreenHouses.get(0),
+                            mockGreenHouses.get(3)
+                    )
+            );
+        } else if (mockToken.equals("2")) {
+            usersGreenHouses.addAll(
+                    Arrays.asList(
+                            mockGreenHouses.get(1),
+                            mockGreenHouses.get(3)
+                    )
+            );
+        } else if (mockToken.equals("3")) {
+            usersGreenHouses.addAll(
+                    Arrays.asList(
+                            mockGreenHouses.get(2),
+                            mockGreenHouses.get(3)
+                    )
+            );
+        } else {
+            usersGreenHouses.addAll(
+                    Arrays.asList(
+                            mockGreenHouses.get(3)
+                    )
+            );
+        }
+
+        return RetrofitUtils.createSuccessCall(usersGreenHouses);
     }
 
     @Override
@@ -66,6 +108,13 @@ public class MockApiService implements ApiService {
 
         Log.d(TAG, "GreenHouse not found");
         return RetrofitUtils.createErrorCall(404, new IOException("GreenHouse not found"));
+    }
+
+    @Override
+    public Call<List<MeasurementModel>> getUserMeasurement(String token) {
+        List<MeasurementModel> mockMeasurement = MockMeasurementRepository.getMeasurement();
+
+        return RetrofitUtils.createSuccessCall(mockMeasurement);
     }
 
     // Utility method to generate a mock token (example implementation)
